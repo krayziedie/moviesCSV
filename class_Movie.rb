ee5f510974581ec92147ddab5992c35f51f32eb2
krayziedie <diego.aguileram@gmail.com>
2015-10-05 13:37:36 -0300

    creation of genre arrays

--- class_Movie.rb
+++ class_Movie.rb
@@ -1,19 +1,54 @@
+# class Movie
+# 	def initialize (dvd_title,studio,released,status,sound,versions,price,rating,year,genre,aspect,upc,dvd_releasedate,id,timestamp)
+# 		@dvd_title = dvd_title	
+# 		@studio = studio
+# 		@released = released
+# 		@status = status
+# 		@sound = sound
+# 		@versions = versions
+# 		@price = price
+# 		@rating = rating
+# 		@year = year
+# 		@genre = genre
+# 		@aspect = aspect
+# 		@upc = upc
+# 		@dvd_releasedate = dvd_releasedate
+# 		@id = id
+# 		@timestamp = timestamp 
+# end
+
 class Movie
-	def initialize (dvd_title,studio,released,status,sound,versions,price,rating,year,genre,aspect,upc,dvd_releasedate,id,timestamp)
-		@dvd_title = dvd_title	
-		@studio = studio
-		@released = released
-		@status = status
-		@sound = sound
-		@versions = versions
-		@price = price
-		@rating = rating
-		@year = year
-		@genre = genre
-		@aspect = aspect
-		@upc = upc
-		@dvd_releasedate = dvd_releasedate
-		@id = id
-		@timestamp = timestamp 
+  attr_reader :genre, :price, :studio
+  def initialize(data = {})
+    data.each { |name, value| instance_variable_set("@#{name}", value) }
+  end
+end
+
+  class Music < Movie
+  def initialize(data = {})
+    super
+    @genre = 'Music'
+  end
+end
+
+class Documentary < Movie
+  def initialize(data = {})
+    super
+    @genre = 'Documentary'
+  end
+end
+
+class Fantasy < Movie
+  def initialize(data = {})
+    super
+    @genre = 'Fantasy'
+  end
+end
+
+class Animation < Movie
+  def initialize(data = {})
+    super
+    @genre = 'Animation'
+  end
 end
 
--- movies_parse.rb
+++ movies_parse.rb
@@ -1,10 +1,16 @@
 require 'csv'
-
+require_relative 'class_Movie.rb'
+#se lee el csv
 movie = []
 csv = CSV.open('dvd_csv_2.csv', headers: true, header_converters: :symbol, converters: :numeric )
 movie = csv.map(&:to_hash)
 
+Music1 = []
+Documentary1 = [] 
+Fantasy1 = []
+Animation1 = []
 
+##borra los campos solicitados
 movie.each do |data|
   data.delete(:status)
   data.delete(:sound)
@@ -18,9 +24,27 @@ movie.each do |data|
   #puts data# show data hash in console
 end
 
+movie.each do |m|
+  case m[:genre]
+  when 'Music'
+    Music1 << Music.new(m)
+  when "Documentary"
+    Documentary1 << Documentary.new(m)
+  when "Fantasy"  
+    Fantasy1 << Fantasy.new(m)
+  when "Animation"
+    Animation1 << Animation.new(m)
+  end
+end
+puts "/////////////////////Categorias/////////////////////////"
+puts "         peliculas musicales : #{Music1.count}"
+puts "         peliculas documentales : #{Documentary1.count}"
+puts "         peliculas de fantasia : #{Fantasy1.count}"
+puts "         peliculas animadas : #{Animation1.count}"
+puts "////////////////////////////////////////////////////////"
 
 
-puts movie.select {|i| i[:dvd_title] == "Connection (2014/ Blu-ray)" }
+#puts movie.select {|i| i[:dvd_title] == "Connection (2014/ Blu-ray)" }
 # hola={}
 # puts movie.select{ |item| item[:genre] == "Comedy" }
 
